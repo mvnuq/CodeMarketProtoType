ff08989f6efe2fe8350557b4c70022a32d85a7f7
mvnuq <121145700+mvnuq@users.noreply.github.com>
2025-06-27 23:34:19 -0400

    testtt

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,33 +1,48 @@
 import React from 'react';
-import { useLocation, Routes, Route } from 'react-router-dom';
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Navigate,
+  useLocation
+} from 'react-router-dom';
 import { AnimatePresence } from 'framer-motion';
-import { AuthProvider }   from './auth/AuthProvider';
-import Login              from './pages/Login';
-import Home               from './pages/Home';
-import Setup              from './pages/Setup';
-import SoftwareDetail     from './pages/SoftwareDetail';
+import { AuthProvider } from './auth/AuthProvider';
+import Login from './pages/Login';
+import Home from './pages/Home';
+import Setup from './pages/Setup';
 import SetupDev from './pages/SetupDev';
 import SuggestionsSoftware from './pages/SuggestionsSoftware';
 import SuggestionsClients from './pages/SuggestionsClients';
+import SoftwareDetail from './pages/SoftwareDetail';
 import SearchResults from './pages/SearchResults';
 
-export default function App() {
+function AnimatedRoutes() {
   const location = useLocation();
 
   return (
-    <AuthProvider>
-      <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/"           element={<Login />} />
-          <Route path="/home"       element={<Home />} />
-          <Route path="/setup"      element={<Setup />} />
-          <Route path="/setup-dev"      element={<SetupDev />} />
-          <Route path="/suggestions-software" element={<SuggestionsSoftware />} />
-          <Route path="/suggestions-clients"  element={<SuggestionsClients />} />
-          <Route path="/software/:id" element={<SoftwareDetail />} />
-          <Route path="/search" element={<SearchResults />} />
-        </Routes>
-      </AnimatePresence>
-    </AuthProvider>
+    <AnimatePresence mode="wait">
+      <Routes location={location} key={location.pathname}>
+        <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/setup" element={<Setup />} />
+        <Route path="/setup-dev" element={<SetupDev />} />
+        <Route path="/suggestions-software" element={<SuggestionsSoftware />} />
+        <Route path="/suggestions-clients" element={<SuggestionsClients />} />
+        <Route path="/software/:id" element={<SoftwareDetail />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="*" element={<Navigate to="/" replace />} />
+      </Routes>
+    </AnimatePresence>
+  );
+}
+
+export default function App() {
+  return (
+    <HashRouter>
+      <AuthProvider>
+        <AnimatedRoutes />
+      </AuthProvider>
+    </HashRouter>
   );
 }
